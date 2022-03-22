ac7bcc21369815a748429639adfc8f398dc31d55
OscarGPDev <oscarprograb@gmail.com>
2022-03-22 14:51:17 -0600

    avance

--- FrontendMision/Semana 4 - Controlando el vuelo con JS/js/main.js
+++ FrontendMision/Semana 4 - Controlando el vuelo con JS/js/main.js
@@ -1,23 +1,55 @@
 const pokeApiUrl = "https://pokeapi.co/api/v2/";
-const getPokemonData = async (pokemonName) =>
-  fetch(`${pokeApiUrl}pokemon/${pokemonName}`).then((res) => res.json())
-  .catch(error=>({requestFailed:true}));
-const setPokemonData = async (pokemonName) => {
-  const imgPokemonNotFound = "./img/404.png";
-  const imgPokemonDisplay = document.getElementById("pokedisplay");
-  const pokemonData = await getPokemonData(pokemonName);
-  console.log(pokemonData);
-  if (pokemonData.requestFailed) {
-    imgPokemonDisplay.src = imgPokemonNotFound;
-    //imgPokemonDisplay.src = pokemonData.sprites.front_default;
-  } else {
-    imgPokemonDisplay.src = pokemonData.sprites.front_default;
+const pokedex = () => {
+  const imageContainer = document.getElementById("pokedisplay-container");
+  const buttons = document.getElementsByClassName("btn");
+  const imageTemplate = "<img class='pokedisplay' src='{imgSrc}' />";
+  const images = {
+    imgPokemonNotFound: "./img/404.png",
+    imgLoading: "./img/404.png",
+  };
+  const colorsByType={
+    
   }
+  const setLoading = () => {
+    imageContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgLoading);
+    for (const button of buttons) {
+      button.disabled = true;
+    }
+  };
+  const setLoadingComplete = () => {
+    for (const button of buttons) {
+      button.disabled = false;
+    }
+  };
+  const getPokemonData = async (pokemonName) =>
+    fetch(`${pokeApiUrl}pokemon/${pokemonName}`)
+      .then((res) => res.json())
+      .catch((error) => ({ requestFailed: true }));
+  const setPokemonData = async (pokemonName) => {
+    setLoading();
+    const pokemonData = await getPokemonData(pokemonName);
+    console.log(pokemonData);
+    if (pokemonData.requestFailed) {
+      imageContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgPokemonNotFound);
+    } else {
+      imageContainer.innerHTML = `
+      ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_default)} 
+      ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_shiny)}
+      `;
+
+      
+    }
+    setLoadingComplete();
+  };
+
+  const triggers = () => {
+    const pokemonInput = document.getElementById("pokemonName");
+    pokemonInput.onchange = (event) => setPokemonData(event.target.value);
+  };
+  return { triggers };
 };
-const triggers = () => {
-  const pokemonInput = document.getElementById("pokemonName");
-  pokemonInput.onchange = (event) => setPokemonData(event.target.value);
-};
+
 window.onload = () => {
-  triggers();
+  const main = pokedex();
+  main.triggers();
 };
