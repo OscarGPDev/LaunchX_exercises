f1c486babf1020acd3b256dd220eec6b6012e5f0
OscarGPDev <oscarprograb@gmail.com>
2022-03-22 22:48:16 -0600

    Finalización de la parte lógica

--- FrontendMision/Semana 4 - Controlando el vuelo con JS/js/main.js
+++ FrontendMision/Semana 4 - Controlando el vuelo con JS/js/main.js
@@ -1,17 +1,83 @@
 const pokeApiUrl = "https://pokeapi.co/api/v2/";
 const pokedex = () => {
   const imageContainer = document.getElementById("pokedisplay-container");
+  const pokemonTypesContainer = document.getElementById("pokemonTypes");
   const buttons = document.getElementsByClassName("btn");
   const imageTemplate = "<img class='pokedisplay' src='{imgSrc}' />";
+  const pokemonStatsElements = {
+    hp: document.getElementById("pokemonStatHp"),
+    attack: document.getElementById("pokemonStatAttack"),
+    defense: document.getElementById("pokemonStatDefense"),
+    specialAttack: document.getElementById("pokemonStatSpecialAttack"),
+    specialDefense: document.getElementById("pokemonStatSpecialDefense"),
+    speed: document.getElementById("pokemonStatSpeed"),
+  };
+  const pokemonNameElement = document.getElementById("pokemonNameResult");
+  const pokemonAbilitiesElement = document.getElementById("pokemonAbilities");
+  const pokemonMovesElement = document.getElementById("pokemonMoves");
+  pokemonAbilities;
   const images = {
     imgPokemonNotFound: "./img/404.png",
     imgLoading: "./img/404.png",
   };
-  const colorsByType={
-    
-  }
+  const processPokemonTypes = (pokemonData) => {
+    let pokemonType = "";
+    pokemonData.types?.forEach((pokemonTypeData) => {
+      pokemonType += `<span class="pokemon-type ${pokemonTypeData.type.name}">${pokemonTypeData.type.name}</span>`;
+    });
+    pokemonTypesContainer.innerHTML = pokemonType;
+  };
+  const processPokemonStats = (pokemonData) => {
+    pokemonData.stats?.forEach((pokemonStatData) => {
+      switch (pokemonStatData.stat.name) {
+        case "hp":
+          pokemonStatsElements.hp.innerHTML = pokemonStatData.base_stat;
+          pokemonStatsElements.hp.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+          break;
+        case "attack":
+          pokemonStatsElements.attack.innerHTML = pokemonStatData.base_stat;
+          pokemonStatsElements.attack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+          break;
+        case "defense":
+          pokemonStatsElements.defense.innerHTML = pokemonStatData.base_stat;
+          pokemonStatsElements.defense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+          break;
+        case "special-attack":
+          pokemonStatsElements.specialAttack.innerHTML =
+            pokemonStatData.base_stat;
+          pokemonStatsElements.specialAttack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+          break;
+        case "special-defense":
+          pokemonStatsElements.specialDefense.innerHTML =
+            pokemonStatData.base_stat;
+          pokemonStatsElements.specialDefense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+          break;
+        case "speed":
+          pokemonStatsElements.speed.innerHTML = pokemonStatData.base_stat;
+          pokemonStatsElements.speed.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+          break;
+      }
+    });
+  };
+  const processPokemonMoves = (pokemonData) => {
+    let pokemonMovesContent = "";
+    pokemonData.moves?.forEach((pokemonMove) => {
+      pokemonMovesContent += `<li>${pokemonMove.move.name}</li>`;
+    });
+    pokemonMovesElement.innerHTML = pokemonMovesContent;
+  };
+  const processPokemonAbilities = (pokemonData) => {
+    let pokemonAbilitiesContent = "";
+    pokemonData.abilities?.forEach((pokemonAbility) => {
+      pokemonAbilitiesContent += `<li>${pokemonAbility.ability.name}</li>`;
+    });
+    pokemonAbilitiesElement.innerHTML = pokemonAbilitiesContent;
+  };
   const setLoading = () => {
-    imageContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgLoading);
+    imageContainer.innerHTML = imageTemplate.replace(
+      "{imgSrc}",
+      images.imgLoading
+    );
     for (const button of buttons) {
       button.disabled = true;
     }
@@ -30,21 +96,29 @@ const pokedex = () => {
     const pokemonData = await getPokemonData(pokemonName);
     console.log(pokemonData);
     if (pokemonData.requestFailed) {
-      imageContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgPokemonNotFound);
+      imageContainer.innerHTML = imageTemplate.replace(
+        "{imgSrc}",
+        images.imgPokemonNotFound
+      );
     } else {
       imageContainer.innerHTML = `
       ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_default)} 
       ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_shiny)}
       `;
+      pokemonNameElement.innerHTML = pokemonData.name;
+      processPokemonTypes(pokemonData);
+      processPokemonStats(pokemonData);
+      processPokemonAbilities(pokemonData);
+      processPokemonMoves(pokemonData);
 
-      
     }
     setLoadingComplete();
   };
 
   const triggers = () => {
     const pokemonInput = document.getElementById("pokemonName");
-    pokemonInput.onchange = (event) => setPokemonData(event.target.value);
+    const btnPokemonSearch = document.getElementById("btnSearch");
+    btnPokemonSearch.onclick = () => setPokemonData(pokemonInput.value);
   };
   return { triggers };
 };
