2011f7c749de2aec492a404582b0da398f37667f
OscarGPDev <oscarprograb@gmail.com>
2022-03-23 15:20:49 -0600

    finalización de la practica de js

--- FrontendMision/Semana 4 - Controlando el vuelo con JS/js/main.js
+++ FrontendMision/Semana 4 - Controlando el vuelo con JS/js/main.js
@@ -15,16 +15,29 @@ const pokedex = () => {
   const pokemonNameElement = document.getElementById("pokemonNameResult");
   const pokemonAbilitiesElement = document.getElementById("pokemonAbilities");
   const pokemonMovesElement = document.getElementById("pokemonMoves");
-  pokemonAbilities;
+  const pokemonIdElement = document.getElementById("pokemonId");
+  const pokemonInput = document.getElementById("pokemonName");
+  const btnPokemonSearch = document.getElementById("btnSearch");
+  const btnPokemonUp = document.getElementById("btnUp");
+  const btnPokemonDown = document.getElementById("btnDown");
+  let currentClassType = null;
   const images = {
     imgPokemonNotFound: "./img/404.png",
-    imgLoading: "./img/404.png",
+    imgLoading: "./img/loading.gif",
   };
   const processPokemonTypes = (pokemonData) => {
     let pokemonType = "";
+    const firstClass = pokemonData.types[0].type.name;
     pokemonData.types?.forEach((pokemonTypeData) => {
       pokemonType += `<span class="pokemon-type ${pokemonTypeData.type.name}">${pokemonTypeData.type.name}</span>`;
     });
+    if (currentClassType) {
+      pokemonMovesElement.classList.remove(currentClassType);
+      pokemonAbilitiesElement.classList.remove(currentClassType);
+    }
+    pokemonMovesElement.classList.add(firstClass);
+    pokemonAbilitiesElement.classList.add(firstClass);
+    currentClassType = firstClass;
     pokemonTypesContainer.innerHTML = pokemonType;
   };
   const processPokemonStats = (pokemonData) => {
@@ -84,42 +97,63 @@ const pokedex = () => {
   };
   const setLoadingComplete = () => {
     for (const button of buttons) {
-      button.disabled = false;
+      checkDisabled(button);
     }
   };
   const getPokemonData = async (pokemonName) =>
     fetch(`${pokeApiUrl}pokemon/${pokemonName}`)
       .then((res) => res.json())
       .catch((error) => ({ requestFailed: true }));
-  const setPokemonData = async (pokemonName) => {
-    setLoading();
-    const pokemonData = await getPokemonData(pokemonName);
-    console.log(pokemonData);
-    if (pokemonData.requestFailed) {
-      imageContainer.innerHTML = imageTemplate.replace(
-        "{imgSrc}",
-        images.imgPokemonNotFound
-      );
+  const checkDisabled = (button) => {
+    if (button.id == "btnDown" && pokemonIdElement.value <= 1) {
+      button.disabled = true;
     } else {
-      imageContainer.innerHTML = `
-      ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_default)} 
-      ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_shiny)}
-      `;
-      pokemonNameElement.innerHTML = pokemonData.name;
-      processPokemonTypes(pokemonData);
-      processPokemonStats(pokemonData);
-      processPokemonAbilities(pokemonData);
-      processPokemonMoves(pokemonData);
-
+      button.disabled = false;
     }
-    setLoadingComplete();
+  };
+  const setPokemonData = async (pokemonName) => {
+    if (pokemonName) {
+      setLoading();
+      const pokemonData = await getPokemonData(pokemonName);
+      if (pokemonData.requestFailed) {
+        imageContainer.innerHTML = imageTemplate.replace(
+          "{imgSrc}",
+          images.imgPokemonNotFound
+        );
+      } else {
+        imageContainer.innerHTML = `
+      
+      
+        ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_default)}
+      
+      
+        ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_shiny)}
+      
+      `;
+        pokemonNameElement.innerHTML = pokemonData.name;
+        pokemonIdElement.value = pokemonData.id;
+        processPokemonTypes(pokemonData);
+        processPokemonStats(pokemonData);
+        processPokemonAbilities(pokemonData);
+        processPokemonMoves(pokemonData);
+      }
+      setLoadingComplete();
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: "Ingresa el nombre de un pokémon primero",
+        icon: "error",
+        confirmButtonText: "Aceptar.",
+      });
+    }
   };
 
   const triggers = () => {
-    const pokemonInput = document.getElementById("pokemonName");
-    const btnPokemonSearch = document.getElementById("btnSearch");
     btnPokemonSearch.onclick = () => setPokemonData(pokemonInput.value);
+    btnPokemonUp.onclick = () => setPokemonData(+pokemonIdElement.value + 1);
+    btnPokemonDown.onclick = () => setPokemonData(+pokemonIdElement.value - 1);
   };
+  setLoadingComplete();
   return { triggers };
 };
 
